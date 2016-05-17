09dfd4c643c151517f9770feea1ba2760780adae
Marko Lainovic <lajka@lajkabaus.com>
2016-05-17 18:25:17 +0200

    added pyOCD binaries and the howto

---
+++ SW/FTF/pyOCD/Scripts/pyocd-flashtool-script.py
@@ -0,0 +1,10 @@
+#!C:\Python27\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pyOCD==0.7.1.dev11','console_scripts','pyocd-flashtool'
+__requires__ = 'pyOCD==0.7.1.dev11'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('pyOCD==0.7.1.dev11', 'console_scripts', 'pyocd-flashtool')()
+    )
