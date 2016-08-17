5e889f99286927e2d5d720170bce0524831ef3f0
ViktorMilovanovic <viktor.milovanovic@mikroe.com>
2016-08-17 09:43:04 +0200

    Updated Create Otap Image files

--- SW/create otap image/otaps_py_MK64_EXAMPLE.bat
+++
@@ -1,1 +0,0 @@
-"c:\Program Files (x86)\Python 3.5\python.exe" otaps.py test_executable.srec imgconfigMK64.json
---
+++ SW/create otap image/otaps_py_example.bat
@@ -0,0 +1,1 @@
+"c:\Program Files (x86)\Python 3.5\python.exe" otaps.py testFW.srec imgconfig.json
